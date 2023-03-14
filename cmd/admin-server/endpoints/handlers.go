c46310f5dd3724dc109afd890e3eea05f949c28b
Jack Ellis <52923466+JackElli@users.noreply.github.com>
2023-03-14 20:51:34 +0000

    Adding Endpoints (#8)
    * Added endpoint struct
    
    * Added endpoints file to hold all endpoints. Added method in main to handle all endpoints. Added new handlers file to house the handler methods
    
    * Added default route for testing

---
+++ cmd/admin-server/endpoints/handlers.go
@@ -0,0 +1,44 @@
+package endpoints
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/LouisHatton/static-site-cms/pkg/files"
+	"github.com/LouisHatton/static-site-cms/pkg/log"
+)
+
+var logger = log.Logger()
+
+// Main route for when user hits /
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		logger.Info("Received main route, for testing")
+	}
+}
+
+// Upload Zip endpoint for when a user uploads their
+// zip containing the HTML files
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		logger.Info("Received file upload request")
+		if err := files.UploadFile(r); err != nil {
+			logger.Error("error parsing uploaded file: ", err.Error())
+			http.Error(w, "error parsing uploaded file", http.StatusInternalServerError)
+			return
+		}
+
+		// Clear the http directory
+		os.RemoveAll("/http/")
+		os.MkdirAll("/http/", 0777)
+
+		if err := files.Unzip("/tmp/upload", "/http/"); err != nil {
+			logger.Error("error unzipping uploaded file: ", err.Error())
+			http.Error(w, "error unzipping uploaded file", http.StatusInternalServerError)
+			return
+		}
+		logger.Info("New files unzipped and deployed in /http")
+	}
+}
--- cmd/admin-server/main.go
+++ cmd/admin-server/main.go
@@ -2,42 +2,20 @@ package main
 
 import (
 	"net/http"
-	"os"
 
-	"github.com/LouisHatton/static-site-cms/pkg/files"
+	"github.com/LouisHatton/static-site-cms/cmd/admin-server/endpoints"
 	"github.com/LouisHatton/static-site-cms/pkg/log"
 )
 
 var logger = log.Logger()
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		logger.Info("Received file upload request")
-		if err := files.UploadFile(r); err != nil {
-			logger.Error("error parsing uploaded file: ", err.Error())
-			http.Error(w, "error parsing uploaded file", http.StatusInternalServerError)
-			return
-		}
-
-		// Clear the http directory
-		os.RemoveAll("/http/")
-		os.MkdirAll("/http/", 0777)
-
-		if err := files.Unzip("/tmp/upload", "/http/"); err != nil {
-			logger.Error("error unzipping uploaded file: ", err.Error())
-			http.Error(w, "error unzipping uploaded file", http.StatusInternalServerError)
-			return
-		}
-		logger.Info("New files unzipped and deployed in /http")
-	}
-}
-
 func main() {
-	// Upload route
-	http.HandleFunc("/upload/full-site", uploadHandler)
+	// Handle all endpoints
+	for _, endpoint := range endpoints.AllEndpoints {
+		endpoint.Handle()
+	}
 
-	//Listen on port 8080
+	// Listen on port 8080
 	logger.Info("Starting server on 0.0.0.0:8080")
 	http.ListenAndServe(":8080", nil)
 }
