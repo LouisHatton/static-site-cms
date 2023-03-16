20d54fd58602717bb5ba1686d4f007f1e6e995e7
Jack Ellis <jackellis100@hotmail.co.uk>
2023-03-16 20:12:52 +0000

    Adding header to admin server UI

--- cmd/admin-ui/src/routes/+layout.svelte
+++ cmd/admin-ui/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import Header from './components/Header.svelte';
 </script>
 
-<slot />
+<Header />
+<div class="mt-16">
+	<slot />
+</div>
---
+++ cmd/admin-ui/src/routes/components/Header.svelte
@@ -0,0 +1,10 @@
+<div class="w-full h-16 bg-slate-800 fixed top-0">
+	<div class="w-[90%] h-full flex mx-auto items-center justify-between text-white">
+		<h1 class="rounded-[3px] text-[20px] bg-[#2f415e] p-1">
+			Re<span class="font-light">factor</span>
+		</h1>
+		<div class="flex items-center gap-1">
+			<p class="hover:underline cursor-pointer">Menu item</p>
+		</div>
+	</div>
+</div>
